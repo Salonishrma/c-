2bcd27b9615a9a5f011f760fb3f807f9602d5b16
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2022-08-26 14:04:32 +0530

    Update month.cpp

--- switch/month.cpp
+++ switch/month.cpp
@@ -1,6 +1,46 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int month=9;
+    switch(month){
+    case 1:
+    cout<<"Janurary";
+    break;
+    case 2:
+    cout<<"February";
+    break;
+    case 3:
+    cout<<"march";
+    break;
+    case 4:
+    cout<<"April";
+    break;
+    case 5:
+    cout<<"may";
+    break;
+    case 6:
+    cout<<"June";
+    break;
+    case 7:
+    cout<<"july";
+    break;
+    case 8:
+    cout<<"August";
+    break;
+    case 9:
+    cout<<"September";
+    break;
+    case 10:
+    cout<<"October";
+    break;
+    case 11:
+    cout<<"November";
+    break;
+    case 12:
+    cout<<"December";
+    }
+    return 0;
 }
